b74d06d99ff1a96ea7f320da07a16fe4a64171fe
LiuWei <987860319@qq.com>
2016-10-01 09:54:58 +0800

    完成了xml数据的获取和解析

--- src/com/dasudian/photogallery/FlickrFetchr.java
+++ src/com/dasudian/photogallery/FlickrFetchr.java
@@ -3,8 +3,14 @@ package com.dasudian.photogallery;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import android.net.Uri;
 import android.util.Log;
@@ -12,12 +18,14 @@ import android.util.Log;
 public class FlickrFetchr {
 
 	public static final String TAG = "FlickrFetchr";
-	private static final String ENDPOINT = "http://api.flickr.com/services/rest/";
+	private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
 	private static final String API_KEY = "57c3aa6dbede1cb5d640a205366fdd92";
 	private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
 	private static final String PARAM_EXTRAS = "extras";
-	private static final String EXTRA_SMALL_URL = "url_s";
-	
+	private static final String EXTRA_SMALL_URL = "url_s";// 如果有小图片，也一并包括在内并返回
+
+	private static final String XML_PHOTO = "photo";
+
 	public byte[] getUrlBytes(String urlSpec) throws IOException {
 		URL url = new URL(urlSpec);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -39,12 +47,14 @@ public class FlickrFetchr {
 			connection.disconnect();
 		}
 	}
-	
+
 	public String getUrl(String urlSpec) throws IOException {
 		return new String(getUrlBytes(urlSpec));
 	}
-	
-	public void fetchItems() {
+
+	public ArrayList<GalleryItem> fetchItems() {
+		ArrayList<GalleryItem> items = new ArrayList<GalleryItem>();
+		
 		try {
 			String url = Uri.parse(ENDPOINT).buildUpon()
 					.appendQueryParameter("method", METHOD_GET_RECENT)
@@ -53,8 +63,36 @@ public class FlickrFetchr {
 					.build().toString();
 			String xmlString = getUrl(url);
 			Log.i(TAG, "Received xml: " + xmlString);
+			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+			XmlPullParser parser = factory.newPullParser();
+			parser.setInput(new StringReader(xmlString));
+			
+			parseItems(items, parser);
 		} catch (IOException ioe) {
 			Log.e(TAG, "Failed to fetch items", ioe);
+		} catch (XmlPullParserException e) {
+			Log.e(TAG, "Failed to parse items", e);
+		}
+		return items;
+	}
+
+	void parseItems(ArrayList<GalleryItem> items, XmlPullParser parser)
+			throws XmlPullParserException, IOException {
+		int eventType = parser.next();
+
+		while (eventType != XmlPullParser.END_DOCUMENT) {
+			if (eventType == XmlPullParser.START_TAG
+					&& XML_PHOTO.equals(parser.getName())) {
+				String id = parser.getAttributeValue(null, "id");
+				String caption = parser.getAttributeValue(null, "title");
+				String smallUrl = parser.getAttributeValue(null, EXTRA_SMALL_URL);
+				GalleryItem item = new GalleryItem();
+				item.setmId(id);
+				item.setmCaption(caption);
+				item.setmUrl(smallUrl);
+				items.add(item);
+			}
+			eventType = parser.next();
 		}
 	}
 }
--- src/com/dasudian/photogallery/PhotoGalleryFragment.java
+++ src/com/dasudian/photogallery/PhotoGalleryFragment.java
@@ -1,21 +1,22 @@
 package com.dasudian.photogallery;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 public class PhotoGalleryFragment extends Fragment {
 	private static final String TAG = "PhotoGalleryFragment";
 	GridView mGridView;
-	
+	ArrayList<GalleryItem> mItems;
+
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,27 +24,42 @@ public class PhotoGalleryFragment extends Fragment {
 		setRetainInstance(true);
 		new FetchItemsTask().execute();
 	}
-	
+
 	@Override
 	@Nullable
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+		View v = inflater.inflate(R.layout.fragment_photo_gallery, container,
+				false);
 		mGridView = (GridView) v.findViewById(R.id.gridView);
+		
+		setupAdapter();
+		
 		return v;
 	}
-	
-	private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+	void setupAdapter() {
+		// 检查getActivity是否为空，因为fragment可以脱离activity存在
+		if (getActivity() == null || mGridView == null)
+			return;
+		if (mItems != null) {
+			mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),
+					android.R.layout.simple_gallery_item, mItems));
+		} else {
+			mGridView.setAdapter(null);
+		}
+	}
+
+	private class FetchItemsTask extends AsyncTask<Void, Void, ArrayList<GalleryItem>> {
 		@Override
-		protected Void doInBackground(Void... params) {
-			String result;
-			try {
-				result = new FlickrFetchr().getUrl("http://www.baidu.com");
-				Log.i(TAG, "Fetched contents of URL: " + result);
-			} catch (IOException e) {
-				Log.e(TAG, "Failed to fetch URL: ", e);
-			}
-			return null;
+		protected ArrayList<GalleryItem> doInBackground(Void... params) {
+			return new FlickrFetchr().fetchItems();
+		}
+		
+		@Override
+		protected void onPostExecute(ArrayList<GalleryItem> items) {
+			mItems = items;
+			setupAdapter();
 		}
 	}
 
